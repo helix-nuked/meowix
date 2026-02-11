9bfdafd0d1416953b20b5102911219e402c3adaf
helix-nuked <helix.nuked@proton.me>
2026-02-11 07:51:01 +0500

    change user configuration

--- configuration.nix
+++ configuration.nix
@@ -68,14 +68,24 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users.alice = {
-  #   isNormalUser = true;
-  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-  #   packages = with pkgs; [
-  #     tree
-  #   ];
-  # };
+  users = {
+    mutableUsers = false;
+    users = {
+      "root" = {
+        isSystemUser = true;
+        hashedPassword = "$6$rx4JIYoJUSzq3v0l$8cevGhPZrNTw0ou/XjmVcki/sfD5.2J9Wub8H2Pt65IjC3xEwdNw5Lfo41bsx3AiMCQbYAeJ0IgKKAGZjtByL1";
+      };
+      # Define a user account.
+      "monyx" = {
+        hashedPassword = "$6$oIE5OhKIdKQ8Iazs$qgd2Tfc5wwmsutOeRFXpb/yzwChYR/EEHtLhKViTQbz.cxAINqdYIq6sFRVot1nfYU2mGlgI4yidyCl9C39cD1";
+        isNormalUser = true;
+        extraGroups = ["wheel"]; # Enable ‘sudo’ for the user.
+        packages = with pkgs; [
+          tree
+        ];
+      };
+    };
+  };
 
   # programs.firefox.enable = true;
 
